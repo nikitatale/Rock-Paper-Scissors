4aa888851a7cad469651d19d9bbe697a585c4499
Nikita Tale <talenikita3@gmail.com>
2025-03-12 22:44:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let userScore = 0;
+let compScore = 0;
+
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice = () => {
+    let options = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random()*3)
+    return options[randomIndex];
+}
+
+const drawGame = () => {
+    msg.innerText = "Game Was Draw. Play Again"
+    msg.style.backgroundColor = "yellow"
+    msg.style.color = "black";
+}
+
+const showWinner = (userWin, userChoice, compChoice) => {
+         if(userWin){
+             userScore++;
+             userScorePara.innerText = userScore;
+             msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`
+             msg.style.backgroundColor = "green"
+            }
+            else{
+                compScore++;
+                compScorePara.innerText = compScore;
+                msg.innerText = `You Lose.${compChoice} beats your ${userChoice}`
+                msg.style.backgroundColor = "red"
+            }
+}
+
+const playGame = (userChoice) => {
+  const compChoice = genCompChoice();
+
+
+  if(userChoice === compChoice){
+    //draw Game
+    drawGame();
+  } else{
+    let userWin = true;
+    if(userChoice === "rock"){
+        //scissors, paper
+        userWin = compChoice === "paper" ? false : true;
+    }
+    else if(userChoice === "paper"){
+        //rock, scissors
+       userWin =  compChoice === "scissors" ? false : true;
+    } else {
+         //rock, paper
+        userWin = compChoice === "rock"? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+}
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    })
+})
+
